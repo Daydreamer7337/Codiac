98de938b5235e13b57fb04ee878e18664579c586
Nikhil_B <87606716+Daydreamer7337@users.noreply.github.com>
2024-10-05 22:55:52 +0530

    Camera Movements completed
    Camera can now be moved to selected Exoplanet.

--- Codiac/codiac/Scripts/PlanetPopup.gd
+++ Codiac/codiac/Scripts/PlanetPopup.gd
@@ -3,6 +3,7 @@ extends Window
 var matches = []
 var planet_names = []
 var button_container = "Window/ScrollContainer/VBoxContainer"
+var file_path = "res://CSVFiles/Exoplanet_Cartesian_Cordinates.txt"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -45,9 +46,26 @@ func _search_query_submit() -> void:
 	for i in matches:
 		var Btn = Button.new()
 		Btn.text = i
+		Btn.connect("button_down", func(): _on_button_pressed(Btn.text))
 		$ScrollContainer/VBoxContainer.add_child(Btn)
 		
 func _clear_list(): #NOT WORKING. To be fixed
 	var children = $ScrollContainer/VBoxContainer.get_children()
 	for child in children:
 		child.free()
+		
+func _on_button_pressed(selected_planet: String) -> void:
+	var x
+	var y
+	var z
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	while not file.eof_reached():
+		var line = file.get_line()  # Read the current line as a string
+		var columns = line.split(",")
+		if columns[0] == selected_planet:  # Compare with the planet_name (assuming it's in the first column)
+			# Extract x, y, z coordinates from the 3rd, 4th, and 5th columns
+			x = float(columns[2])  # 3rd column
+			y = float(columns[3])  # 4th column
+			z = float(columns[4])  # 5th column
+	file.close()
+	%Camera3D.position = Vector3(x,y,z)
