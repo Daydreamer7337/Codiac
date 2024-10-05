000d30414f528c8e2525e5a562b04cdf0fe6fc20
Nikhil_B <87606716+Daydreamer7337@users.noreply.github.com>
2024-10-05 17:45:49 +0530

    Completed Search panel
    Added search, reset to the exoplanet search window. Search options are not connected.

--- Codiac/codiac/Scripts/PlanetPopup.gd
+++ Codiac/codiac/Scripts/PlanetPopup.gd
@@ -1,8 +1,14 @@
 extends Window
 
+var matches = []
+var planet_names = []
+var button_container = "Window/ScrollContainer/VBoxContainer"
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.visible = false  # Ensure the popup starts hidden
+	load_planet_names("res://CSVFiles/Exoplanet_Cartesian_Cordinates.txt")
 
 # Function to open the popup
 func open_popup() -> void:
@@ -11,3 +17,37 @@ func open_popup() -> void:
 
 func close_window() -> void:
 	self.visible = false
+
+
+func load_planet_names(file_path: String):
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	if file != null:
+		while not file.eof_reached():
+			var line = file.get_line()
+			var columns = line.split(",")  # Split the line by commas (CSV format)
+			if columns.size() > 0 and columns[0] != "pl_name":  # Avoid the header
+				planet_names.append(columns[0])  # Append the first column (planet names)
+		file.close()
+
+
+func _search_query_submit() -> void:
+	_clear_list()
+	matches = []
+	
+	var query = $LineEdit.text
+	
+	if query == " ":
+		return
+	
+	for i in planet_names:
+		if query in i:
+			matches.append(i)
+	for i in matches:
+		var Btn = Button.new()
+		Btn.text = i
+		$ScrollContainer/VBoxContainer.add_child(Btn)
+		
+func _clear_list(): #NOT WORKING. To be fixed
+	var children = $ScrollContainer/VBoxContainer.get_children()
+	for child in children:
+		child.free()
